fdf9c77d0e4d64d0d38032f268aa0bdb44997023
YEKabantsov <tesnus288@gmail.com>
2018-01-24 23:41:56 +0300

    config file and new event in db

--- myscript.py
+++ myscript.py
@@ -5,32 +5,43 @@ import logging
 import select
 import yaml
 import re
+import smtplib
+import socket
+from email.message import EmailMessage
 from systemd import journal
 from systemd.journal import JournalHandler
 
+conf = yaml.load(open('conf.yaml'))
+print(conf)
+print(conf[0]['email'])
 
+def send_email(message, level, from_who):
+    print('input function emai')
+    me = from_who
+#    you = 'yuriy@fastzone.ru'
+    you = conf[0]['email']
+    print(you + 'meil from config')
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = 'Level' + level
+    msg['From'] = me
+    msg['To'] = you
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    print('output function emai')
+    s.quit()
 
-#import smtplib   
-#s = smtplib.SMTP("localhost")
-#tolist = ["yuriy@fastzone.ru","tesnus288@gmail.com"]
-#msg = '''
-#... From:kaze28@yandex.ru
-#... Subject: testin ...
-#...
-#... This is a test'''
-#s.sendmail("me@my.org", tolist, msg)
-#{"yuriy@fastzone.ru" : (550 ,"User unknown")}
-#s.quit()
-
-def test_event (myevent):    
+def test_event (myevent, level):    
     j = journal.Reader()
     j.this_boot()
-    j.log_level(journal.LOG_INFO)
+#    j.log_level(journal.LOG_INFO)
+    level
+#    j.log_level(journal.level)
     j.add_match(MESSAGE = myevent)
     for entry in j:                                 
         print(str(entry['__REALTIME_TIMESTAMP'] )+ ' ' + entry['MESSAGE'])
 
-def output_event_in_real_time (massevent):
+def output_event_in_real_time(massevent):
     j = journal.Reader()
     j.log_level(journal.LOG_INFO)
     j.seek_tail()
@@ -44,20 +55,19 @@ def output_event_in_real_time (massevent):
                 event = re.compile(massevent[i]['regexp'])
                 result  = event.findall(entry['MESSAGE'])
                 if result:
-#                    print(str(entry['__REALTIME_TIMESTAMP']) + ' ' + result[0])
                     if (massevent[i]['happened'] + 1) >= (massevent[i]['limit']):
                         print(str(entry['__REALTIME_TIMESTAMP']) + ' ' + result[0])
-                        print('Send message administrator')
+                        from_who = socket.gethostbyname(socket.gethostname())
+#                        send_email(str(entry['__REALTIME_TIMESTAMP']) + ' ' + result[0], massevent[i]['priority'], from_who)
+                        print('Send message administrator from: ' + from_who)
                         massevent[i]['happened'] == 0
-#                        print(result)
                     else:
                         massevent[i]['happened'] += 1
                 i += 1
 
+#send_email('send message', 'red', 'yuriy@fastzone.ru')
 
-#            print(str(entry['__REALTIME_TIMESTAMP'] )+ ' ' + entry['MESSAGE'])
-
-
-cfg = yaml.load(open('database.yaml'))
-
-output_event_in_real_time (cfg)
+level = conf[0]['email']
+test_event('fatal: no SASL authentication mechanisms', level)
+#cfg = yaml.load(open('database.yaml'))
+#output_event_in_real_time (cfg)
